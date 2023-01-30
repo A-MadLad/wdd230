2c7202ec08cfc0b7c5a12965236ef8634771435b
A-MadLad <adamjudd07@gmail.com>
2023-01-29 22:01:47 -0700

    added date and hamburger icon

---
+++ chamber/scripts/script.js
@@ -0,0 +1,33 @@
+let d = new Date();
+let year = d.getFullYear();
+
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+
+document.querySelector("#date").textContent = fulldate;
