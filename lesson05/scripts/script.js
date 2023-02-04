373ebb507885d2197181167970141c08be0003bd
A-MadLad <adamjudd07@gmail.com>
2023-02-03 19:53:34 -0700

    favicon

--- lesson05/scripts/script.js
+++ lesson05/scripts/script.js
@@ -4,16 +4,6 @@ const button = document.querySelector("#add");
 const list = document.querySelector("#list");
 
 // Create an click event listener for the Add Chapter button using addEventListener and an anonymous function.
-button.addEventListener('click', function () {
-    function required() {
-        const input = document.querySelector("#favchap").value;
-        if (input == "") {
-            alert("Please input a Value");
-            return false;
-        }
-        else {
-            alert('Code has accepted : you can try another');
-            return true;
-        }
-    }
-});
+button.addEventListener('click', function() {
+    
+})
