05ed074475e5d2a19c523fe9cae3c0f5a15ad0ff
A-MadLad <adamjudd07@gmail.com>
2023-02-05 22:39:39 -0700

    n/a

--- lesson05/scripts/script.js
+++ lesson05/scripts/script.js
@@ -5,5 +5,11 @@ const list = document.querySelector("#list");
 
 // Create an click event listener for the Add Chapter button using addEventListener and an anonymous function.
 button.addEventListener('click', function() {
-    
+    const li = document.createElement('li');
+    const del_button = document.createElement('button');
+    li.textContent = input;
+    del_button.textContent = 'X';
+    li.append(del_button);
+    list.append(li);
+    del_button.addEventListener('click', li.remove);
 })
