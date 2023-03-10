1aa83e638a8a8ddab5613c47f0cfaef7a2f12f7c
A-MadLad <adamjudd07@gmail.com>
2023-03-09 22:10:03 -0700

    n/a

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -5,32 +5,32 @@ const captionDesc = document.querySelector('figcaption');
 const url = 'http://api.openweathermap.org/data/2.5/forecast?q=Fairbanks&units=imperial&appid=458f4ec6f678f2377b139701bacd2ef2';
 async function apiFetch() {
     try {
-      const response = await fetch(url);
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data); // this is for testing the call
-        // displayResults(data);
-      } else {
-          throw Error(await response.text());
-      }
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // this is for testing the call
+            // displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
     } catch (error) {
         console.log(error);
     }
-  }
-  
-  apiFetch();
+}
 
-  function displayResults(weatherData) {
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-  }
+apiFetch();
 
-  function  displayResults(weatherData) {
+function displayResults(weatherData) {
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-  
+}
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
-  
+
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = desc;
-  }
+}
