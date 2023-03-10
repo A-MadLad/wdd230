00e50c09b2b83e09355ff83c20c614257b757530
A-MadLad <adamjudd07@gmail.com>
2023-03-09 22:35:23 -0700

    updates

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -2,12 +2,13 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'http://api.openweathermap.org/data/2.5/forecast?q=Fairbanks&units=imperial&appid=458f4ec6f678f2377b139701bacd2ef2';
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=458f4ec6f678f2377b139701bacd2ef2';
 async function apiFetch() {
     try {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
+            console.log(data);
             displayResults(data);
         } else {
             throw Error(await response.text());
