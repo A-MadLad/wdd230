3267a5ad48e591c429ae3982d79c7d1dc3da3787
A-MadLad <adamjudd07@gmail.com>
2023-03-13 11:29:03 -0600

    updates

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,24 +1,10 @@
-// let temperature = 45;
-// let speed = 17;
-// let chill = document.getElementById("wind_chill");
-
-
-// function windchill(){
-//     if(temperature <= 50 && speed > 3.0){
-//         let w_chill = 35.74 + 0.6215 * temperature - 35.75 * speed ** 0.16 + 0.4275 * temperature * speed ** 0.16
-//         let round_chill = Math.round(w_chill *10) / 10;
-//         chill.append(`Wind Chill: ${round_chill}`);
-//     }
-//     else{
-//         chill.append("N/A");
-//     } 
-// };
-
-// windchill();
+// Temperature
 
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+const windSpeed = document.querySelector('#speed');
+const windChill = document.querySelector('#chill');
 
 const url2 = 'https://api.openweathermap.org/data/2.5/weather?q=Alamogordo&units=imperial&appid=458f4ec6f678f2377b139701bacd2ef2';
 async function apiFetch() {
@@ -39,11 +25,8 @@ async function apiFetch() {
 apiFetch();
 
 function displayResults(weatherData) {
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-};
-
-function displayResults(weatherData) {
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    currentTemp.innerHTML = `${weatherData.main.temp.toFixed(0)}`;
+    windSpeed.innerHTML = `${weatherData.wind.speed.toFixed(0)}`;
 
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
@@ -51,4 +34,21 @@ function displayResults(weatherData) {
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = desc;
-}
+};
+
+
+// Windchill
+
+function windchill() {
+    if (currentTemp <= 50 && windSpeed > 3.0) {
+        let w_chill = 35.74 + 0.6215 * currentTemp - 35.75 * windSpeed ** 0.16 + 0.4275 * currentTemp * windSpeed ** 0.16
+        let round_chill = Math.round(w_chill * 10) / 10;
+        windChill.append(`Wind Chill: ${round_chill}`);
+    }
+    else {
+        windChill.append("N/A");
+    }
+};
+
+windchill();
+
